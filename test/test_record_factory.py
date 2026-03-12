f247c2e61fb471f4108218527e348f2421354570
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2026-03-12 13:54:19 +0000

    Remove duplicated code and fix redeine of 'type'

--- test/test_record_factory.py
+++ test/test_record_factory.py
@@ -121,111 +121,6 @@ class TestRecordFactory(unittest.TestCase):
         """Test the expected error is raised on handling malformed JSON."""
         malformed_json = '{"Key": "Value}'
 
-        with self.assertRaisesRegex(RecordFactoryException,
-                                     "^Malformed JSON:"):
-            self._rf.create_records(malformed_json)
-
-    def test_create_records_json_unknown_type(self):
-        """Test the expected error is raised on messages without a type."""
-        unknown_type_json = '{"Key": "Value"}'
-
-        with self.assertRaisesRegex(RecordFactoryException,
-                                     "^Type of JSON message not provided."):
-            self._rf.create_records(unknown_type_json)
-
-    def test_create_records_json_unsupported_type(self):
-        """Test the expected error is raised on unsupported message types."""
-        unsupported_json = self._generate_json_message(
-            "Spammy Type", "ignored version", "[{'Key': 'Value'}]"
-        )
-
-        with self.assertRaisesRegex(RecordFactoryException,
-                                     "^Unsupported JSON message type:"):
-            self._rf.create_records(unsupported_json)
-
-    def test_create_records_accelerator(self):
-        """Test creation of AcceleratorRecords from a JSON message."""
-        # Create some records for a test message.
-        record0 = {
-            "MeasurementMonth": 9,
-            "MeasurementYear": 2018,
-            "AssociatedRecordType": "cloud",
-            "AssociatedRecord": "TEST-FakeVMUUID",
-            "GlobalUserName": "GlobalUser1",
-            "FQAN": "FQAN1",
-            "SiteName": "TEST-SITE",
-            "Count": 0.5,
-            "Cores": 64,
-            "ActiveDuration": 600,
-            "AvailableDuration": 6000,
-            "BenchmarkType": "BenchmarkType1",
-            "Benchmark": 1005,
-            "Type": "GPU",
-            "Model": "Model1",
-        }
-
-        record1 = {
-            "MeasurementMonth": 9,
-            "MeasurementYear": 2018,
-            "AssociatedRecordType": "cloud",
-            "AssociatedRecord": "TEST-FakeVMUUID2",
-            "GlobalUserName": "GlobalUser2",
-            "FQAN": "FQAN2",
-            "SiteName": "TEST-SITE",
-            "Count": 1,
-            "Cores": 8,
-            "ActiveDuration": 30,
-            "AvailableDuration": 600,
-            "BenchmarkType": "BenchmarkType2",
-            "Benchmark": 995,
-            "Type": "FPGA",
-            "Model": "Model2",
-        }
-
-        # Create a list of records and build a Accelerator accounting message.
-        usage_records = [record0, record1]
-        accelerator_accounting_message_v00 = self._generate_json_message(
-            "APEL-Accelerator-message", "0.1", usage_records
-        )
-
-        # Create the record objects, using the RecordFactory.
-        record_object_list = self._rf.create_records(accelerator_accounting_message_v00)
-
-        # Compare the generated record objects with the dictionary provided
-        # as input.
-        self._compare_record_to_dictionary(record_object_list[0], record0)
-        self._compare_record_to_dictionary(record_object_list[1], record1)
-
-    def _generate_json_message(self, type, version, usage_records):
-        """Helper function to generate JSON messages."""
-        message = JSON_MSG_BOILERPLATE % (type, version, usage_records)
-        # The above will put single quotes into the created message, but
-        # JSON is strictly double quotes. So replace single quotes.
-        message = message.replace("'", '"')
-        return message
-
-    def _compare_record_to_dictionary(self, record_object, dictionary):
-        """Helper function to compare a record object and a dictionary."""
-        # Loop through the record content of the record object.
-        for key, value in record_object._record_content.items():
-            # Compare the value in the record object to the corresponding key
-            # in the dictionary. Handle datetime fields differently because in
-            # the message they are expressed as seconds-since-epoch, but in the
-            # object they are datetime objects.
-            if key in record_object._datetime_fields:
-                self.assertEqual(
-                    value,
-                    datetime.utcfromtimestamp(dictionary[key])
-                )
-            # This test fails when the local clock uses daylight savings time.
-            else:
-                self.assertEqual(value, dictionary[key])
-
-
-    def test_create_records_json_malformed(self):
-        """Test the expected error is raised on handling malformed JSON."""
-        malformed_json = '{"Key": "Value}'
-
         with self.assertRaisesRegex(RecordFactoryException, "^Malformed JSON:"):
             self._rf.create_records(malformed_json)
 
@@ -300,9 +195,9 @@ class TestRecordFactory(unittest.TestCase):
         self._compare_record_to_dictionary(record_object_list[0], record0)
         self._compare_record_to_dictionary(record_object_list[1], record1)
 
-    def _generate_json_message(self, type, version, usage_records):
+    def _generate_json_message(self, header, version, usage_records):
         """Helper function to generate JSON messages."""
-        message = JSON_MSG_BOILERPLATE % (type, version, usage_records)
+        message = JSON_MSG_BOILERPLATE % (header, version, usage_records)
         # The above will put single quotes into the created message, but
         # JSON is strictly double quotes. So replace single quotes.
         message = message.replace("'", '"')
